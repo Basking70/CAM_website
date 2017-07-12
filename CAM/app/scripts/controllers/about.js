bc0c828dc234c9ec1d49bcc7dd7db3c70217761b
Basking70 <navab@usc.edu>
2017-07-12 12:50:52 -0700

    changing the location of web_app to CAM project

---
+++ CAM/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name camApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the camApp
+ */
+angular.module('camApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ CAM/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name camApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the camApp
+ */
+angular.module('camApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
