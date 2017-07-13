69a07c42c0f4490da269d3024fbd212151646638
Basking70 <navab@usc.edu>
2017-07-13 14:07:01 -0700

    I finished the design of the equipment page

--- CAM/app/scripts/controllers/equipment.js
+++ CAM/app/scripts/controllers/equipment.js
@@ -14,4 +14,19 @@ angular.module('camApp')
       'AngularJS',
       'Karma'
     ];
+
+	$(document).ready(function(){
+    
+	$("#myBtn1").click(function(){
+        $("#myModal1").modal({backdrop: "static"});
+    });
+	$("#myBtn2").click(function(){
+        $("#myModal2").modal({backdrop: "static"});
+    });	
+	$("#myBtn3").click(function(){
+        $("#myModal3").modal({backdrop: "static"});
+    });
+	});
+	
+	
   });
