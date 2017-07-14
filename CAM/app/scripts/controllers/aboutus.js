485e91214de53cffacfc55a1a9bb8569e74e5123
Basking70 <navab@usc.edu>
2017-07-13 19:15:20 -0700

    finished the style of the aboutUS page

--- CAM/app/scripts/controllers/aboutus.js
+++ CAM/app/scripts/controllers/aboutus.js
@@ -14,4 +14,45 @@ angular.module('camApp')
       'AngularJS',
       'Karma'
     ];
+	/// the first 	
+	$(document).ready(function(){
+    $("#flip1").click(function(){
+        $("#panel1").slideToggle("slow");
+    	});
+	//// the secound
+	    $("#flip2").click(function(){
+        $("#panel2").slideToggle("slow");
+    	});
+	//// the third
+		$("#flip3").click(function(){
+        $("#panel3").slideToggle("slow");
+    	});
+	//// the forth
+		$("#flip4").click(function(){
+        $("#panel4").slideToggle("slow");
+    	});
+	//// the fifth
+		$("#flip5").click(function(){
+        $("#panel5").slideToggle("slow");
+    	});
+	//// the sith
+		$("#flip6").click(function(){
+        $("#panel6").slideToggle("slow");
+    	});
+	//// the seveth
+		$("#flip7").click(function(){
+        $("#panel7").slideToggle("slow");
+    	});
+
+		$('.collapse').on('shown.bs.collapse', function(){
+		$(this).parent().find(".glyphicon-plus").removeClass("glyphicon-plus").addClass("glyphicon-minus");
+		}).on('hidden.bs.collapse', function(){
+		$(this).parent().find(".glyphicon-minus").removeClass("glyphicon-minus").addClass("glyphicon-plus");
+		});
+		
+		
+		
+		
+		
+	});
   });
