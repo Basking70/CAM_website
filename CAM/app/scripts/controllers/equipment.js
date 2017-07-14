0b55a844e9c2b96ec4d267b982b35d354ac9599a
Basking70 <navab@usc.edu>
2017-07-14 15:06:51 -0700

    finish the Robots section

--- CAM/app/scripts/controllers/equipment.js
+++ CAM/app/scripts/controllers/equipment.js
@@ -16,17 +16,36 @@ angular.module('camApp')
     ];
 
 	$(document).ready(function(){
-    
-	$("#myBtn1").click(function(){
-        $("#myModal1").modal({backdrop: "static"});
-    });
-	$("#myBtn2").click(function(){
-        $("#myModal2").modal({backdrop: "static"});
-    });	
-	$("#myBtn3").click(function(){
-        $("#myModal3").modal({backdrop: "static"});
-    });
+  
+		$("#myBtn1").click(function(){
+			$("#myModal1").modal({backdrop: "static"});
+		});
+		$("#myBtn2").click(function(){
+			$("#myModal2").modal({backdrop: "static"});
+		});	
+		$("#myBtn3").click(function(){
+			$("#myModal3").modal({backdrop: "static"});
+		});
+		$("#myBtn4").click(function(){
+			$("#myModal4").modal({backdrop: "static"});
+		});
+		$("#myBtn5").click(function(){
+			$("#myModal5").modal({backdrop: "static"});
+		});
+		$("#myBtn6").click(function(){
+			$("#myModal6").modal({backdrop: "static"});
+		});
+		$("#myBtn7").click(function(){
+			$("#myModal7").modal({backdrop: "static"});
+		});
+		$("#myBtn8").click(function(){
+			$("#myModal8").modal({backdrop: "static"});
+		});
+		$("#myBtn9").click(function(){
+			$("#myModal9").modal({backdrop: "static"});
+		});
+		$("#myBtn10").click(function(){
+			$("#myModal10").modal({backdrop: "static"});
+		});
 	});
-	
-	
   });
