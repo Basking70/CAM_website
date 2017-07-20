3f75557b63f36386879d45df4a44c669cb339c6d
Basking70 <navab@usc.edu>
2017-07-20 16:09:51 -0700

    added 10 faculty to people page

--- CAM/app/scripts/controllers/people.js
+++ CAM/app/scripts/controllers/people.js
@@ -17,11 +17,92 @@ angular.module('camApp')
 	
 	
 	$(document).ready(function(){
-    
-	$("#myBtn1").click(function(){
-        $("#myModal1").modal({backdrop: "static"});
-    });
-    });
+  	/// btn1
+		$("#myBtn1").click(function(){
+			$("#myModal1").modal({backdrop: "static"});
+		});
+  	/// btn2
+		$("#myBtn2").click(function(){
+			$("#myModal2").modal({backdrop: "static"});
+		});
+  	/// btn3
+		$("#myBtn3").click(function(){
+			$("#myModal3").modal({backdrop: "static"});
+		});
+  	/// btn4
+		$("#myBtn4").click(function(){
+			$("#myModal4").modal({backdrop: "static"});
+		});
+  	/// btn5
+		$("#myBtn5").click(function(){
+			$("#myModal5").modal({backdrop: "static"});
+		});
+  	/// btn6
+		$("#myBtn6").click(function(){
+			$("#myModal6").modal({backdrop: "static"});
+		});
+  	/// btn7
+		$("#myBtn7").click(function(){
+			$("#myModal7").modal({backdrop: "static"});
+		});
+  	/// btn8
+		$("#myBtn8").click(function(){
+			$("#myModal8").modal({backdrop: "static"});
+		});
+  	/// btn9
+		$("#myBtn9").click(function(){
+			$("#myModal9").modal({backdrop: "static"});
+		});
+  	/// btn10
+		$("#myBtn10").click(function(){
+			$("#myModal10").modal({backdrop: "static"});
+		});
+  	/// btn11
+		$("#myBtn11").click(function(){
+			$("#myModal11").modal({backdrop: "static"});
+		});
+  	/// btn12
+		$("#myBtn12").click(function(){
+			$("#myModal12").modal({backdrop: "static"});
+		});
+  	/// btn13
+		$("#myBtn13").click(function(){
+			$("#myModal13").modal({backdrop: "static"});
+		});
+  	/// btn14
+		$("#myBtn14").click(function(){
+			$("#myModal14").modal({backdrop: "static"});
+		});
+  	/// btn15
+		$("#myBtn15").click(function(){
+			$("#myModal15").modal({backdrop: "static"});
+		});
+  	/// btn16
+		$("#myBtn16").click(function(){
+			$("#myModal16").modal({backdrop: "static"});
+		});
+  	/// btn17
+		$("#myBtn17").click(function(){
+			$("#myModal17").modal({backdrop: "static"});
+		});
+  	/// btn18
+		$("#myBtn18").click(function(){
+			$("#myModal18").modal({backdrop: "static"});
+		});
+  	/// btn19
+		$("#myBtn19").click(function(){
+			$("#myModal19").modal({backdrop: "static"});
+		});
+  	/// btn20
+		$("#myBtn20").click(function(){
+			$("#myModal20").modal({backdrop: "static"});
+		});
 
-	
+
+		
+		
+		
+		
+		
+    });
   });
