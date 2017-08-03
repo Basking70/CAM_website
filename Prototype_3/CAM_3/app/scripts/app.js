a08ccb43e09c6253303f5e7ffffa0c589aa8b7b1
Basking70 <navab@usc.edu>
2017-08-02 19:30:43 -0700

    created new pages

---
+++ Prototype_3/CAM_3/app/scripts/app.js
@@ -0,0 +1,81 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name camApp
+ * @description
+ * # camApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('camApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+	
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/research', {
+        templateUrl: 'views/research.html',
+        controller: 'ResearchCtrl',
+        controllerAs: 'research'
+      })
+      .when('/seminars', {
+        templateUrl: 'views/seminars.html',
+        controller: 'SeminarsCtrl',
+        controllerAs: 'seminars'
+      })
+      .when('/equipment', {
+        templateUrl: 'views/equipment.html',
+        controller: 'EquipmentCtrl',
+        controllerAs: 'equipment'
+      })
+      .when('/publications', {
+        templateUrl: 'views/publications.html',
+        controller: 'PublicationsCtrl',
+        controllerAs: 'publications'
+      })	
+      .when('/aboutus', {
+        templateUrl: 'views/aboutus.html',
+        controller: 'AboutusCtrl',
+        controllerAs: 'aboutus'
+      })
+      .when('/people', {
+        templateUrl: 'views/people.html',
+        controller: 'PeopleCtrl',
+        controllerAs: 'people'
+      })
+      .when('/gallery', {
+        templateUrl: 'views/gallery.html',
+        controller: 'GalleryCtrl',
+        controllerAs: 'gallery'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'login'
+      })
+      .when('/admin', {
+        templateUrl: 'views/admin.html',
+        controller: 'AdminCtrl',
+        controllerAs: 'admin'
+      })
+      .when('/user', {
+        templateUrl: 'views/user.html',
+        controller: 'UserCtrl',
+        controllerAs: 'user'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
