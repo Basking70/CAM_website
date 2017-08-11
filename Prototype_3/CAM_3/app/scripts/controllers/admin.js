73bbfd948f1f68c8d9e6fc200ee57986c4a653c1
Basking70 <navab@usc.edu>
2017-08-10 18:10:33 -0700

    Get Data from Jason file and show it

--- Prototype_3/CAM_3/app/scripts/controllers/admin.js
+++ Prototype_3/CAM_3/app/scripts/controllers/admin.js
@@ -8,10 +8,36 @@
  * Controller of the camApp
  */
 angular.module('camApp')
-  .controller('AdminCtrl', function () {
+  .controller('AdminCtrl', function ($scope,$http) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+	
+	
+		//Get Data from Jason file
+	$http.get("DB/research.php").then(function (response) {
+      	$scope.reData = response.data.records;
+		
+		$scope.ReLength = Object.keys(response).length; // double check
+	
+		
+
+		
+  	});
+	
+	
+	
+	
+		$(document).ready(function(){
+		/// btn1
+			$("#myBtn1").click(function(){
+				$("#myModal1").modal({backdrop: "static"});
+			});
+		/// btn2
+			$("#myBtn2").click(function(){
+				$("#myModal2").modal({backdrop: "static"});
+			});
+		});
   });
