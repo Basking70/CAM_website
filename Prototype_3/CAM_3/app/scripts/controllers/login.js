f4fe8b0262a49b850a5aa06355e43747a5081c3f
Basking70 <navab@usc.edu>
2017-08-11 18:26:59 -0700

    developed controller for login

--- Prototype_3/CAM_3/app/scripts/controllers/login.js
+++ Prototype_3/CAM_3/app/scripts/controllers/login.js
@@ -8,10 +8,72 @@
  * Controller of the camApp
  */
 angular.module('camApp')
-  .controller('LoginCtrl', function () {
+  .controller('LoginCtrl', function ($scope , $location, $timeout) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+	
+$scope.formSubmit = function() {
+	$scope.test = true;
+	if ($scope.username == "admin" && $scope.password == "pass")
+		{
+			$scope.error = " ";
+			$scope.ErrAlert = false;
+			$location.path('/admin');
+		}
+		else
+		{
+			$scope.test = false;
+			$scope.error = "Incorrect username/password !";
+			$scope.ErrAlert = true;
+			
+			$timeout(function () {
+				$scope.ErrAlert = false;
+			}, 1000);
+		};
+};
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	
+    $scope.formSubmit = function() {
+		// timeout for loading icon
+		$scope.test = true;
+		$timeout(function () {			
+				if ($scope.username == "admin" && $scope.password == "pass")
+					{
+						$scope.error = " ";
+						$scope.ErrAlert = false;
+						$location.path('/admin');
+					}
+				else
+					{
+						$scope.error = "Incorrect username/password !";
+						$scope.ErrAlert = true;
+					}	
+	    		$scope.myHeader = "How are you today?";
+				$scope.test = false;
+  			}, 1000);	
+		};
+  
+*/
+
+
+
+
+});
