291892ed5bb9cd2589376e53ca88c955a1ebc355
Basking70 <navab@usc.edu>
2017-08-24 16:50:23 -0700

    created other pages

--- Prototype_3/CAM_3/Front-end/app/scripts/controllers/admin.js
+++ Prototype_3/CAM_3/Front-end/app/scripts/controllers/admin.js
@@ -223,7 +223,7 @@ angular.module('camApp')
 							method : "GET",
 							url : "http://localhost:5000/api/equipment",
 						}).then(function mySuccess(response) {
-							console.log("I got the data for Equipment I requested");
+							console.log("I got the data for Equipment page I requested");
 							$scope.EquipmentData = response.data;
 						}, function myError(response) {
 							$scope.myWelcome = "Something went wrong";
@@ -372,7 +372,7 @@ angular.module('camApp')
 							method : "GET",
 							url : "http://localhost:5000/api/People",
 						}).then(function mySuccess(response) {
-							console.log("I got the data for People I requested");
+							console.log("I got the data for People page I requested");
 							$scope.PeopleData = response.data;
 						}, function myError(response) {
 							$scope.myWelcome = "Something went wrong";
@@ -501,8 +501,407 @@ angular.module('camApp')
 	
 /////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////// END OF EQUIPMENT PAGE //////////////////////////////////////
+//////////////////////////// END OF People PAGE //////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////
+
+//---------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////// START OF Gallery PAGE ////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+		// Start of the whole process of adding a new pic	
+			//-------------------------
+				var Refresh_Gallery = function(){
+					// get data from db and show it to the table
+						$http({
+							method : "GET",
+							url : "http://localhost:5000/api/Gallery",
+						}).then(function mySuccess(response) {
+							console.log("I got the data for Gallery page I requested");
+							$scope.GalleryData = response.data;
+						}, function myError(response) {
+							$scope.myWelcome = "Something went wrong";
+						});
+				};
+			// load data when we get the page
+				Refresh_Gallery();
+
+			//-------------------------
+			// open the form for adding a new pic
+				$scope.OpenAddGalleryForm = function(Id)
+				{
+					$("#Gallery1").modal({backdrop: "static"});
+
+				};
+			//-------------------------
+			// Clear the input in add a new pic
+				$scope.ClearAddGallery = function()
+				{
+					console.log("cleared!");
+					$scope.Gallery.Id="";
+					$scope.Gallery.Name="";
+					$scope.Gallery.PicUrl="";
+				};
+			//-------------------------
+			// add a new pic to the db
+				$scope.AddNewGallery= function(){
+					console.log($scope.Gallery);
+
+					/// uploading file to the folder
+					   var file = $scope.myFile;
+
+					   console.log('file is ' );
+					   console.log(file);
+
+					$http({
+						method : "POST",
+						url : "http://localhost:5000/api/Gallery",
+						data: $scope.Gallery
+					}).then(function mySuccess(response) {
+						console.log(response);
+						Refresh_Gallery();
+						SuccessMsg("The new pic was added to the system!"); // showing the message 
+					}, function myError(response) {
+						console.log("Something went wrong");
+					});			
+
+
+				};	
+			//-------------------------
+		// End of the whole process of adding a new picture		
+
+		/////////////////////////////////////////////////////////////////////////////////////////
+
+		// Start of the whole process of Remove a new pic	
+			//-------------------------	
+			//sending the *Remove* request to the server
+				// request to remove
+				var Selected_Gallery_For_Deleting;
+				$scope.RemoveReq_Gallery = function(id){
+					console.log(id);
+					Selected_Gallery_For_Deleting = id;
+					$("#Gallery3").modal({backdrop: "static"});
+
+			};
+				// removing the Gallery
+				$scope.Remove_Gallery = function(){
+					console.log(Selected_Gallery_For_Deleting);
+
+					$http({
+						method : "DELETE",
+						url : 'http://localhost:5000/api/Gallery/' + Selected_Gallery_For_Deleting
+					}).then(function mySuccess(response) {
+						Refresh_Gallery(); // refreshing the page after deleting successfully
+						SuccessMsg("The pic was deleted from the system!"); // showing the message 
+					}, function myError(response) {
+						$scope.myWelcome = "Something went wrong";
+					});			
+
+				};	
+			//-------------------------
+		// End of the whole process of *Remove* a new pic	
+		
 	
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////// END OF Gallery PAGE //////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+//---------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////// START OF News PAGE ////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+		// Start of the whole process of adding a new News	
+			//-------------------------
+				var Refresh_News = function(){
+					// get data from db and show it to the table
+						$http({
+							method : "GET",
+							url : "http://localhost:5000/api/News",
+						}).then(function mySuccess(response) {
+							console.log("I got the data for News page I requested");
+							$scope.NewsData = response.data;
+						}, function myError(response) {
+							$scope.myWelcome = "Something went wrong";
+						});
+				};
+			// load data when we get the page
+				Refresh_News();
+
+			//-------------------------
+			// open the form for adding a new Equipment
+				$scope.OpenAddNewsForm = function(Id)
+				{
+					$("#News1").modal({backdrop: "static"});
+
+				};
+			//-------------------------
+			// Clear the input in add a new Equipment
+				$scope.ClearAddNews = function()
+				{
+					console.log("cleared!");
+					$scope.News.Id="";
+					$scope.News.Caption="";
+					$scope.News.PicUrl="";
+				};
+			//-------------------------
+			// add a new News to the db
+				$scope.AddNewNews = function(){
+					console.log($scope.News);
+
+					/// uploading file to the folder
+					   var file = $scope.myFile;
+
+					   console.log('file is ' );
+					   console.log(file);
+
+					$http({
+						method : "POST",
+						url : "http://localhost:5000/api/News",
+						data: $scope.News
+					}).then(function mySuccess(response) {
+						console.log(response);
+						Refresh_News();
+						SuccessMsg("The new News was added to the system!"); // showing the message 
+					}, function myError(response) {
+						console.log("Something went wrong");
+					});			
+
+
+				};	
+			//-------------------------
+		// End of the whole process of adding a new News		
+
+		/////////////////////////////////////////////////////////////////////////////////////////
+
+		// Start of the whole process of Remove a new News	
+			//-------------------------	
+			//sending the *Remove* request to the server
+				// request to remove
+				var Selected_News_For_Deleting;
+				$scope.RemoveReq_News = function(id){
+					console.log(id);
+					Selected_News_For_Deleting = id;
+					$("#News3").modal({backdrop: "static"});
+
+			};
+				// removing the News
+				$scope.Remove_News = function(){
+					console.log(Selected_News_For_Deleting);
+
+					$http({
+						method : "DELETE",
+						url : 'http://localhost:5000/api/News/' + Selected_News_For_Deleting
+					}).then(function mySuccess(response) {
+						Refresh_News(); // refreshing the page after deleting successfully
+						SuccessMsg("The Person was deleted from the system!"); // showing the message 
+					}, function myError(response) {
+						$scope.myWelcome = "Something went wrong";
+					});			
+
+				};	
+			//-------------------------
+		// End of the whole process of *Remove* a new News	
+
+		/////////////////////////////////////////////////////////////////////////////////////////
+
+		// Start of the whole process of *Update* a new News	
+			//-------------------------	
+			//getting the exact Person that we want to edit
+			$scope.edit_News = function(id){
+				console.log(id);
+						$http({
+							method : 'GET',
+							url : 'http://localhost:5000/api/News/' + id
+						}).then(function mySuccess(response) {
+							$("#News2").modal({backdrop: "static"});
+							$scope.News = response.data;
+						}, function myError(response) {
+							$scope.myWelcome = "Something went wrong";
+						});
+			};
+			// save the edit and send the changes to server
+			$scope.updateNews = function(){
+				console.log($scope.News._id);// this put put the Equipment id to the console 
+						$http({
+							method : 'PUT',
+							url : 'http://localhost:5000/api/News/' + $scope.News._id, 
+							data: $scope.News
+						}).then(function mySuccess(response) {
+							Refresh_News();
+							SuccessMsg("The News was updated!"); // showing the message 
+							$scope.News = response.data;
+						}, function myError(response) {
+							console.log("Something went wrong");
+							console.log($scope.News);
+						});
+			};
+			//-------------------------
+		// End of the whole process of *Update* a new News		
+	
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////// END OF News PAGE /////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+//---------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////// START OF Publication PAGE ////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+		// Start of the whole process of adding a new Publication	
+			//-------------------------
+				var Refresh_Publication = function(){
+					// get data from db and show it to the table
+						$http({
+							method : "GET",
+							url : "http://localhost:5000/api/publication",
+						}).then(function mySuccess(response) {
+							console.log("I got the data for Publication page I requested");
+							$scope.PublicationData = response.data;
+						}, function myError(response) {
+							$scope.myWelcome = "Something went wrong";
+						});
+				};
+			// load data when we get the page
+				Refresh_Publication();
+
+			//-------------------------
+			// open the form for adding a new Equipment
+				$scope.OpenAddPublicationForm = function(Id)
+				{
+					$("#Publication1").modal({backdrop: "static"});
+
+				};
+			//-------------------------
+			// Clear the input in add a new Equipment
+				$scope.ClearAddPublication = function()
+				{
+					console.log("cleared!");
+					$scope.Publication.Id="";
+					$scope.Publication.Authors="";
+					$scope.Publication.Name="";
+					$scope.Publication.Location="";
+					$scope.Publication.Year="";
+				};
+			//-------------------------
+			// add a new Equipment to the db
+				$scope.AddNewPublication = function(){
+					console.log($scope.Publication);
+
+					/// uploading file to the folder
+					   var file = $scope.myFile;
+
+					   console.log('file is ' );
+					   console.log(file);
+
+					$http({
+						method : "POST",
+						url : "http://localhost:5000/api/publication",
+						data: $scope.Publication
+					}).then(function mySuccess(response) {
+						console.log(response);
+						Refresh_Publication();
+						SuccessMsg("The new Publication was added to the system!"); // showing the message 
+					}, function myError(response) {
+						console.log("Something went wrong");
+					});			
+
+
+				};	
+			//-------------------------
+		// End of the whole process of adding a new Publication		
+
+		/////////////////////////////////////////////////////////////////////////////////////////
+
+		// Start of the whole process of Remove a new Publication	
+			//-------------------------	
+			//sending the *Remove* request to the server
+				// request to remove
+				var Selected_Publication_For_Deleting;
+				$scope.RemoveReq_Publication = function(id){
+					console.log(id);
+					Selected_Publication_For_Deleting = id;
+					$("#Publication3").modal({backdrop: "static"});
+
+			};
+				// removing the Publication
+				$scope.Remove_Publication = function(){
+					console.log(Selected_Publication_For_Deleting);
+
+					$http({
+						method : "DELETE",
+						url : 'http://localhost:5000/api/publication/' + Selected_Publication_For_Deleting
+					}).then(function mySuccess(response) {
+						Refresh_Publication(); // refreshing the page after deleting successfully
+						SuccessMsg("The new Publication was deleted from the system!"); // showing the message 
+					}, function myError(response) {
+						$scope.myWelcome = "Something went wrong";
+					});			
+
+				};	
+			//-------------------------
+		// End of the whole process of *Remove* a new Publication	
+
+		/////////////////////////////////////////////////////////////////////////////////////////
+
+		// Start of the whole process of *Update* a new Publication	
+			//-------------------------	
+			//getting the exact Equipment that we want to edit
+			$scope.edit_Publication = function(id){
+				console.log(id);
+						$http({
+							method : 'GET',
+							url : 'http://localhost:5000/api/publication/' + id
+						}).then(function mySuccess(response) {
+							$("#Publication2").modal({backdrop: "static"});
+							$scope.Publication = response.data;
+						}, function myError(response) {
+							$scope.myWelcome = "Something went wrong";
+						});
+			};
+			// save the edit and send the changes to server
+			$scope.update_Publication = function(){
+				console.log($scope.Publication._id);// this put put the Publication id to the console 
+						$http({
+							method : 'PUT',
+							url : 'http://localhost:5000/api/publication/' + $scope.Publication._id, 
+							data: $scope.Publication
+						}).then(function mySuccess(response) {
+							Refresh_Publication();
+							SuccessMsg("The Publication was updated!"); // showing the message 
+							$scope.Publication = response.data;
+						}, function myError(response) {
+							console.log("Something went wrong");
+							console.log($scope.Publication);
+						});
+			};
+			//-------------------------
+		// End of the whole process of *Update* a new Publication		
+	
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////// END OF Publication PAGE //////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
 });
