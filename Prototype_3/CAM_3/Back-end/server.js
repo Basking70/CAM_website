0cc81a990c9aea699fbffda1026d6ab26f7cd761
Basking70 <navab@usc.edu>
2017-08-23 20:00:21 -0700

    created the server side of the people page

--- Prototype_3/CAM_3/Back-end/server.js
+++ Prototype_3/CAM_3/Back-end/server.js
@@ -12,7 +12,9 @@ var bodyParser = require('body-parser');
 	//which mongo db and collection are we going to use db1 for using database for research 
 		var db1= mangojs('research', ['research']);
 	//which mongo db and collection are we going to use db1 for using database for equipment
-		var db2= mangojs('equipment', ['equipment']);
+		var db2= mangojs('equipment', ['equipment']);	
+	//which mongo db and collection are we going to use db1 for using database for equipment
+		var db3= mangojs('people', ['people']);
 
 	//var database;
 	//-------------------------
@@ -151,6 +153,64 @@ var bodyParser = require('body-parser');
 			console.log(req.body.Name);
 			db2.equipment.findAndModify({query: {_id: mangojs.ObjectId(id)},// select the research that we want to modify
 				update: {$set: {Id: req.body.Id, Group: req.body.Group, PicUrl: req.body.PicUrl, CompanyName: req.body.CompanyName, Name: req.body.Name, ReadMore: req.body.ReadMore, Content: req.body.Content }},
+			new: true}, function(err,doc){
+				res.json(doc);
+			});
+		});
+
+//---------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// People PAGE ///////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+	// getting data from db and send it tp the front end to show on the html file
+		app.get('/api/people', function(req,res){
+
+			console.log("I recived a GET request!");
+			db3.people.find(function(err,docs){
+				console.log(docs); // make sure we have recived the data
+				res.json(docs);// send the data back to the controller 
+			});
+		});
+	//----------------------------------
+	// listen data front end and save it to the database 
+		app.post('/api/people', function(req,res){
+			console.log(req.body); // show the inserted data to consol, for server to parse the body and we installed body-parser which we required on the top
+			// insert data to db
+			db3.people.insert(req.body, function(err,doc){
+				res.json(doc);
+			})
+		});
+	//----------------------------------
+	// getting the delete request from controller and delete it from server
+		app.delete('/api/people/:id', function(req, res){
+			var id = req.params.id; //  get the value of the id from url
+			console.log(id);// show which reaserch with id is gooing to be deleted
+			db3.people.remove({_id: mangojs.ObjectId(id)}, function(err,doc){
+				res.json(doc); // send back the item that we removed from db to the admin controller
+			})
+		});
+	//----------------------------------
+	// getting the update request from controller and delete it from server
+		app.get('/api/people/:id', function(req,res){
+			var id = req.params.id; //  get the value of the id from url
+			console.log(id);// show which reaserch with id is gooing to be edited
+			db3.people.findOne({_id: mangojs.ObjectId(id)}, function(err,doc){
+				res.json(doc); // send back the item that we removed from db to the admin controller
+			})
+
+		});
+	//----------------------------------
+	// update the people fild in the 
+		app.put('/api/people/:id', function(req,res){
+			//console.log(id);
+			var id = req.params.id; //  get the value of the id from url
+			console.log(req.body.Name);
+			db3.people.findAndModify({query: {_id: mangojs.ObjectId(id)},// select the research that we want to modify
+				update: {$set: {Id: req.body.Id, Group: req.body.Group, Name: req.body.Name, Title: req.body.Title, ResearchArea: req.body.ResearchArea, CurrentProjects: req.body.CurrentProjects, Email: req.body.Email, Phone: req.body.Phone, Website: req.body.Website, PicUrl: req.body.PicUrl }},
 			new: true}, function(err,doc){
 				res.json(doc);
 			});
