daaabfc62a148da2d94cd54dddc6b2649884417d
Basking70 <navab@usc.edu>
2017-08-23 20:00:33 -0700

    created the people side for admin

--- Prototype_3/CAM_3/Front-end/app/scripts/controllers/admin.js
+++ Prototype_3/CAM_3/Front-end/app/scripts/controllers/admin.js
@@ -352,5 +352,157 @@ angular.module('camApp')
 //////////////////////////// END OF EQUIPMENT PAGE //////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////
+
+
+//---------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////// START OF People PAGE ////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+		// Start of the whole process of adding a new Person	
+			//-------------------------
+				var Refresh_People = function(){
+					// get data from db and show it to the table
+						$http({
+							method : "GET",
+							url : "http://localhost:5000/api/People",
+						}).then(function mySuccess(response) {
+							console.log("I got the data for People I requested");
+							$scope.PeopleData = response.data;
+						}, function myError(response) {
+							$scope.myWelcome = "Something went wrong";
+						});
+				};
+			// load data when we get the page
+				Refresh_People();
+
+			//-------------------------
+			// open the form for adding a new Equipment
+				$scope.OpenAddPeopleForm = function(Id)
+				{
+					$("#People1").modal({backdrop: "static"});
+
+				};
+			//-------------------------
+			// Clear the input in add a new Equipment
+				$scope.ClearAddPeople = function()
+				{
+					console.log("cleared!");
+					$scope.People.Id="";
+					$scope.People.Group="";
+					$scope.People.Name="";
+					$scope.People.Title="";
+					$scope.People.ResearchArea="";
+					$scope.People.CurrentProjects="";
+					$scope.People.Email="";
+					$scope.People.Phone="";
+					$scope.People.Website="";
+					$scope.People.PicUrl="";
+				};
+			//-------------------------
+			// add a new People to the db
+				$scope.AddNewPeople = function(){
+					console.log($scope.People);
+
+					/// uploading file to the folder
+					   var file = $scope.myFile;
+
+					   console.log('file is ' );
+					   console.log(file);
+
+					$http({
+						method : "POST",
+						url : "http://localhost:5000/api/People",
+						data: $scope.People
+					}).then(function mySuccess(response) {
+						console.log(response);
+						Refresh_People();
+						SuccessMsg("The new Person was added to the system!"); // showing the message 
+					}, function myError(response) {
+						console.log("Something went wrong");
+					});			
+
+
+				};	
+			//-------------------------
+		// End of the whole process of adding a new Person		
+
+		/////////////////////////////////////////////////////////////////////////////////////////
+
+		// Start of the whole process of Remove a new Person	
+			//-------------------------	
+			//sending the *Remove* request to the server
+				// request to remove
+				var Selected_People_For_Deleting;
+				$scope.RemoveReq_People = function(id){
+					console.log(id);
+					Selected_People_For_Deleting = id;
+					$("#People3").modal({backdrop: "static"});
+
+			};
+				// removing the People
+				$scope.Remove_People = function(){
+					console.log(Selected_People_For_Deleting);
+
+					$http({
+						method : "DELETE",
+						url : 'http://localhost:5000/api/People/' + Selected_People_For_Deleting
+					}).then(function mySuccess(response) {
+						Refresh_People(); // refreshing the page after deleting successfully
+						SuccessMsg("The Person was deleted from the system!"); // showing the message 
+					}, function myError(response) {
+						$scope.myWelcome = "Something went wrong";
+					});			
+
+				};	
+			//-------------------------
+		// End of the whole process of *Remove* a new Person	
+
+		/////////////////////////////////////////////////////////////////////////////////////////
+
+		// Start of the whole process of *Update* a new Person	
+			//-------------------------	
+			//getting the exact Person that we want to edit
+			$scope.edit_People = function(id){
+				console.log(id);
+						$http({
+							method : 'GET',
+							url : 'http://localhost:5000/api/People/' + id
+						}).then(function mySuccess(response) {
+							$("#People2").modal({backdrop: "static"});
+							$scope.People = response.data;
+						}, function myError(response) {
+							$scope.myWelcome = "Something went wrong";
+						});
+			};
+			// save the edit and send the changes to server
+			$scope.update_People = function(){
+				console.log($scope.People._id);// this put put the Equipment id to the console 
+						$http({
+							method : 'PUT',
+							url : 'http://localhost:5000/api/People/' + $scope.People._id, 
+							data: $scope.People
+						}).then(function mySuccess(response) {
+							Refresh_People();
+							SuccessMsg("The People was updated!"); // showing the message 
+							$scope.People = response.data;
+						}, function myError(response) {
+							console.log("Something went wrong");
+							console.log($scope.People);
+						});
+			};
+			//-------------------------
+		// End of the whole process of *Update* a new People		
+	
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////// END OF EQUIPMENT PAGE //////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 	
 });
