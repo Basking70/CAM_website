b36f02354c38c5249593e17f80c4794d686973e6
Basking70 <navab@usc.edu>
2017-08-24 16:49:59 -0700

    developed other pages

--- Prototype_3/CAM_3/Back-end/server.js
+++ Prototype_3/CAM_3/Back-end/server.js
@@ -15,6 +15,12 @@ var bodyParser = require('body-parser');
 		var db2= mangojs('equipment', ['equipment']);	
 	//which mongo db and collection are we going to use db1 for using database for equipment
 		var db3= mangojs('people', ['people']);
+	//which mongo db and collection are we going to use db1 for using database for equipment
+		var db4= mangojs('gallery', ['gallery']);
+	//which mongo db and collection are we going to use db1 for using database for equipment
+		var db5= mangojs('news', ['news']);	
+	//which mongo db and collection are we going to use db1 for using database for equipment
+		var db6= mangojs('publication', ['publication']);
 
 	//var database;
 	//-------------------------
@@ -216,5 +222,166 @@ var bodyParser = require('body-parser');
 			});
 		});
 
+//---------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
+		
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// Gallery PAGE /////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+	// getting data from db and send it tp the front end to show on the html file
+		app.get('/api/gallery', function(req,res){
 
+			console.log("I recived a GET request!");
+			db4.gallery.find(function(err,docs){
+				console.log(docs); // make sure we have recived the data
+				res.json(docs);// send the data back to the controller 
+			});
+		});
+	//----------------------------------
+	// listen data front end and save it to the database 
+		app.post('/api/gallery', function(req,res){
+			console.log(req.body); // show the inserted data to consol, for server to parse the body and we installed body-parser which we required on the top
+			// insert data to db
+			db4.gallery.insert(req.body, function(err,doc){
+				res.json(doc);
+			})
+		});
+	//----------------------------------
+	// getting the delete request from controller and delete it from server
+		app.delete('/api/gallery/:id', function(req, res){
+			var id = req.params.id; //  get the value of the id from url
+			console.log(id);// show which reaserch with id is gooing to be deleted
+			db4.gallery.remove({_id: mangojs.ObjectId(id)}, function(err,doc){
+				res.json(doc); // send back the item that we removed from db to the admin controller
+			})
+		});
+	//----------------------------------
+	// getting the update request from controller and delete it from server
+		app.get('/api/gallery/:id', function(req,res){
+			var id = req.params.id; //  get the value of the id from url
+			console.log(id);// show which reaserch with id is gooing to be edited
+			db4.gallery.findOne({_id: mangojs.ObjectId(id)}, function(err,doc){
+				res.json(doc); // send back the item that we removed from db to the admin controller
+			})
+
+		});
+
+
+//---------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// News PAGE ///////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+	// getting data from db and send it tp the front end to show on the html file
+		app.get('/api/news', function(req,res){
+
+			console.log("I recived a GET request!");
+			db5.news.find(function(err,docs){
+				console.log(docs); // make sure we have recived the data
+				res.json(docs);// send the data back to the controller 
+			});
+		});
+	//----------------------------------
+	// listen data front end and save it to the database 
+		app.post('/api/news', function(req,res){
+			console.log(req.body); // show the inserted data to consol, for server to parse the body and we installed body-parser which we required on the top
+			// insert data to db
+			db5.news.insert(req.body, function(err,doc){
+				res.json(doc);
+			})
+		});
+	//----------------------------------
+	// getting the delete request from controller and delete it from server
+		app.delete('/api/news/:id', function(req, res){
+			var id = req.params.id; //  get the value of the id from url
+			console.log(id);// show which reaserch with id is gooing to be deleted
+			db5.news.remove({_id: mangojs.ObjectId(id)}, function(err,doc){
+				res.json(doc); // send back the item that we removed from db to the admin controller
+			})
+		});
+	//----------------------------------
+	// getting the update request from controller and delete it from server
+		app.get('/api/news/:id', function(req,res){
+			var id = req.params.id; //  get the value of the id from url
+			console.log(id);// show which reaserch with id is gooing to be edited
+			db5.news.findOne({_id: mangojs.ObjectId(id)}, function(err,doc){
+				res.json(doc); // send back the item that we removed from db to the admin controller
+			})
+
+		});
+	//----------------------------------
+	// update the news fild in the 
+		app.put('/api/news/:id', function(req,res){
+			//console.log(id);
+			var id = req.params.id; //  get the value of the id from url
+			console.log(req.body.Name);
+			db5.news.findAndModify({query: {_id: mangojs.ObjectId(id)},// select the research that we want to modify
+				update: {$set: {Id: req.body.Id, Caption: req.body.Caption, PicUrl: req.body.PicUrl }},
+			new: true}, function(err,doc){
+				res.json(doc);
+			});
+		});
+
+//---------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// Publication PAGE ////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+	// getting data from db and send it tp the front end to show on the html file
+		app.get('/api/publication', function(req,res){
+
+			console.log("I recived a GET request!");
+			db6.publication.find(function(err,docs){
+				console.log(docs); // make sure we have recived the data
+				res.json(docs);// send the data back to the controller 
+			});
+		});
+	//----------------------------------
+	// listen data front end and save it to the database 
+		app.post('/api/publication', function(req,res){
+			console.log(req.body); // show the inserted data to consol, for server to parse the body and we installed body-parser which we required on the top
+			// insert data to db
+			db6.publication.insert(req.body, function(err,doc){
+				res.json(doc);
+			})
+		});
+	//----------------------------------
+	// getting the delete request from controller and delete it from server
+		app.delete('/api/publication/:id', function(req, res){
+			var id = req.params.id; //  get the value of the id from url
+			console.log(id);// show which reaserch with id is gooing to be deleted
+			db6.publication.remove({_id: mangojs.ObjectId(id)}, function(err,doc){
+				res.json(doc); // send back the item that we removed from db to the admin controller
+			})
+		});
+	//----------------------------------
+	// getting the update request from controller and delete it from server
+		app.get('/api/publication/:id', function(req,res){
+			var id = req.params.id; //  get the value of the id from url
+			console.log(id);// show which reaserch with id is gooing to be edited
+			db6.publication.findOne({_id: mangojs.ObjectId(id)}, function(err,doc){
+				res.json(doc); // send back the item that we removed from db to the admin controller
+			})
+
+		});
+	//----------------------------------
+	// update the reaserch fild in the 
+		app.put('/api/publication/:id', function(req,res){
+			//console.log(id);
+			var id = req.params.id; //  get the value of the id from url
+			console.log(req.body.Name);
+			db6.publication.findAndModify({query: {_id: mangojs.ObjectId(id)},// select the research that we want to modify
+				update: {$set: {Id: req.body.Id, Authors: req.body.Authors, Name: req.body.Name, Location: req.body.Location, Year: req.body.Year }},
+			new: true}, function(err,doc){
+				res.json(doc);
+			});
+		});
 
